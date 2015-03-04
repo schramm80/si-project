5c69258fb50f9d17d5f941f2fe96d9a53b0628a3
Schramm <carlos.schramm80@gmail.com>
2015-03-04 17:55:02 +0100

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,20 @@
+class Owner
+  
+  def name
+    name = 'Jimmy Kraken'
+  end
+  
+  def birthdate
+    birthdate = Date.new(2010, 3, 1)
+  end
+  
+  def countdown
+    today = Date.today
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    if birthday > today
+      countdown = (birthday - today).to_i
+    else
+      countdown = (birthday.next_year - today).to_i
+    end
+  end
+end
